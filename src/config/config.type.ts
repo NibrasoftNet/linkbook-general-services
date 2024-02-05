a05337272d6a3a32cfdbf65efb026d6a25543aee
SofienRogue <sofien.abidi@digitalroguewave.com>
2024-02-05 08:26:05 +0100

    Add files modules

--- src/config/config.type.ts
+++ src/config/config.type.ts
@@ -1,8 +1,10 @@
 import { AppConfig } from './app-config.type';
 import { MailerConfig } from '../mail/config/mailer-config.type';
+import { FileConfig } from '../files/config/file-config.type';
 
 
 export type ServiceConfigType = {
     app: AppConfig;
     mail: MailerConfig;
+    file: FileConfig;
 };
